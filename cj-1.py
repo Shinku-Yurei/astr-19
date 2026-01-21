98cd30032249b429fa8a433fe1f7f5f3ab21294e
Shinku-Yurei <gedian54@gmail.com>
2026-01-21 00:13:06 -0800

    Adding Code Journal 1 Prompt 1

---
+++ cj-1.py
@@ -0,0 +1,7 @@
+print("Tymari Austin")
+
+he_him = ("He","Him")
+
+print(he_him)
+
+print("I don't really have a favorite movie, but my favorite space movie is Interstellar. My favorite food is Shrimp Alfredo.")
